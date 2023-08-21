634f2b31e0a94c46df6c7884867c154518348c89
aqibaliakbar <aqibaliakbar@gmail.com>
2023-08-21 06:28:58 +0500

    Comment deleted!

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { store } from './app/store';
-import { Provider } from 'react-redux';
-import { fetchPosts } from './features/posts/postsSlice';
-import { fetchUsers } from './features/users/usersSlice';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
+import { fetchPosts } from "./features/posts/postsSlice";
+import { fetchUsers } from "./features/users/usersSlice";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-store.dispatch(fetchPosts()); // we are loading posts immediately as the app load
+store.dispatch(fetchPosts());
 store.dispatch(fetchUsers());
 
 ReactDOM.render(
@@ -16,8 +16,7 @@ ReactDOM.render(
     <Provider store={store}>
       <BrowserRouter>
         <Routes>
-          <Route path="/*" element={<App />} />{" "}
-          {/* Path is important this /* allow for nested routes when we get to App.js */}
+          <Route path="/*" element={<App />} />
         </Routes>
       </BrowserRouter>
     </Provider>
